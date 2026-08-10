badecca79547ee27ecb2b7a2595b8e9da0e667ba
Irwin Felicien <felicien.irwin@gmail.com>
2026-08-10 10:48:22 -0500

    Refactor code structure for improved readability and maintainability

--- obj/Debug/net10.0/SH Entry Portal.AssemblyInfo.cs
+++ obj/Debug/net10.0/SH Entry Portal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SH_Entry_Portal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cebb52bdd0c31f42c937051a52a90968865b31e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b8b6c4ab0b95868296feb73b87ed2006ba441aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("SH_Entry_Portal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SH_Entry_Portal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
